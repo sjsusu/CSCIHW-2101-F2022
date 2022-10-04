6b17d1a892f33f8bae2a9f38858b2666de1f88f5
Gregory J-ctr Walsh <gregory.j-ctr.walsh@faa.gov>
2022-10-04 14:52:53 -0400

    added homework 1

---
+++ HW1/Main.java
@@ -0,0 +1,52 @@
+package HW1;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Start of HW1");
+
+        // This is our Main Method. Any code we write here will be executed when we run the program.
+        // Write some code here to print out "Hello World" to the console.
+
+        // Write some code here to print out the sum of 2 and 3 to the console.
+
+        // You will see another file called Pyramid.java
+        // Write some code here to call the Output method from the Pyramid class.
+        // Note: Since Pyramid is a static method you will need to call it like this:
+        // Class.Method()
+        // Note2: Look at Pyramid.java for more information
+
+
+        // Now look at the next file called Adlibs.java
+        // Write some code here to call the story method from the Adlibs class.
+        // Note: Since story is not a static method you will need to create an object of the Adlibs class
+        //       and then call the method on that object.
+        // Remember to create an object like this:
+        // ClassName ObjectName = new ClassName();
+        // Remember to call objects like this:
+        // Object.Method()
+        // Note2: the story method returns a String type so you will
+        //       need to print out the result of the method call.
+        // Note3: Look at Adlibs.java for more information
+
+
+        // Finally look at File called Replace.java. This is a little more difficult.
+        // Write some code here to call the charReplace method from the Replace class.
+        // Note: Since charReplace is not a static method you will need to create an object of the Replace class
+        //       and then call the method on that object.
+        // Remember to create an object like this:
+        // ClassName ObjectName = new ClassName();
+        // Remember to call objects like this:
+        // Object.Method()
+        // Note2: the charReplace method returns a String type so you will
+        //       need to print out the result of the method call.
+        // Note3: Look at Replace.java for more information
+        
+
+
+
+
+
+
+
+    }
+}
---
+++ HW1/Replace.java
@@ -0,0 +1,22 @@
+package HW1;
+// Make a method that accepts a string and an integer and a char and will return the string with the char
+// replaced at the integer given
+// charReplace("I MAKE BREAD",2,T)
+// EX: I MAKE BREAD, 2, T
+//     I TAKE BREAD
+// remember computer counting starts at 0:
+// HELLO
+// 01234
+// Hint1: The objects name is Replace and you require a string, int, and char
+// Hint2: You will need to return a string. You can concat the variables and the strings together
+// Hint3: You will need to use the substring method to get the first part of the string and the last part of the string
+//        and then concat them together with the char in the middle
+// Hint4: You will need to use the charAt method to get the char at the given index
+// Hint5: You will need to use the length method to get the length of the string
+
+
+public class Replace {
+    public String charReplace(String str, int index, char c) {
+        return null; // replace null with your code
+    }
+}
