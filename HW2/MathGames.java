7f8969d592d9d03786d5e25bf44208a69b22b83e
sjsusu <sus@go.stockton.edu>
2022-10-22 13:28:26 -0400

    HW2

--- HW2/MathGames.java
+++ HW2/MathGames.java
@@ -5,6 +5,23 @@ import java.util.Scanner;
 public class MathGames {
     Scanner scan = new Scanner(System.in);
 
+    // Asks for a non-negative (>=0) double from the user
+    double askDouble(String prompt, String lessThanZeroMessage) {
+        double num;
+
+        do {
+            System.out.print(prompt);
+            num = scan.nextDouble();
+
+            if (num < 0) {
+                System.out.println(lessThanZeroMessage);
+            }
+
+        } while (num < 0);
+
+        return num;
+    }
+
     // 1. Create a method that will calculate the Pythagorean Theorem
     // (https://en.wikipedia.org/wiki/Pythagorean_theorem)
     // c = sqrt(a^2+b^2)
@@ -16,9 +33,13 @@ public class MathGames {
     // Bonus points: Try to make it accept any 2 sides and return the 3rd. So it can
     // take in
     // Hypotenuse and Perpendicular and return the base.
+
     double askSide(char letter) {
-        System.out.print("side " + letter + ": ");
-        double length = scan.nextInt();
+        String prompt = "side " + letter + ": ";
+        String lessThanZeroMessage = "You can't have a negative length. Try again.";
+
+        double length = askDouble(prompt, lessThanZeroMessage);
+
         return length;
     }
 
@@ -27,14 +48,19 @@ public class MathGames {
 
         if (a != 0 && b != 0 && c != 0) {
             System.out.println("Please enter no more than 2 side lengths.");
+
         } else if (a == 0 && b == 0 && c == 0) {
             System.out.println("Please enter 2 side lengths.");
-        } else if ((a == 0 && b == 0) || (a == 0 && b == 0) || (b == 0 && c == 0)) {
+
+        } else if ((a == 0 && b == 0) || (a == 0 && c == 0) || (b == 0 && c == 0)) {
             System.out.println("Please enter one more side length.");
+
         } else if (c != 0) {
             missingSide = Math.sqrt(Math.pow(c, 2) - Math.pow(a, 2) - Math.pow(b, 2));
+
         } else {
             missingSide = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
+
         }
 
         return missingSide;
@@ -55,18 +81,36 @@ public class MathGames {
     // least 5 if / ifelse / else statments
     // Hint 2: You will probably need to cast the double to an int
     // Hint 3: You will probably need to use the Math.ceil() method
-    double askNum(){
-        System.out.print("Please enter a number grade: ");
-        double num = scan.nextDouble();
+
+    double askNum() {
+        String prompt = "Please enter a number grade: ";
+        String lessThanZeroMessage = "You can't have a negative grade. Try agian.";
+
+        double num = askDouble(prompt, lessThanZeroMessage);
 
         return num;
     }
 
     char calcGrade(double numGrade) {
         char grade = 'F';
-        
-    }
+        int num = (int) Math.ceil(numGrade);
 
+        if (num >= 90) {
+            grade = 'A';
+
+        } else if (num >= 80 && num <= 89) {
+            grade = 'B';
+
+        } else if (num >= 70 && num <= 79) {
+            grade = 'C';
+
+        } else if (num >= 60 && num <= 69) {
+            grade = 'D';
+
+        }
+
+        return grade;
+    }
 
     // 3. Create a method that will calculate the tip on a bill per person
     // Method parameters: total bill, amount of people splitting the bill,
@@ -78,5 +122,51 @@ public class MathGames {
     // Hint 3: It is possible you might need to Cast a double to an int or vice
     // versa
     // Hint 4: You will probably need to use the Math.ceil() method
+
+    double askBill() {
+        String prompt = "Bill total: $";
+        String lessThanZeroMessage = "You can't have a negative total. Try again.";
+
+        double total = askDouble(prompt, lessThanZeroMessage);
+
+        return total;
+    }
+
+    int askNumPeople() {
+        int numPeople;
+
+        do {
+            System.out.print("Number of People Paying: ");
+            numPeople = scan.nextInt();
+
+            if (numPeople == 0) {
+                System.out.println("You must have at least one person paying. Try again.");
+
+            } else if (numPeople < 0) {
+                System.out.println("You can't have negative people. Try again.");
+
+            }
+
+        } while (numPeople < 1);
+
+        return numPeople;
+    }
+
+    double askpercentTip() {
+        String prompt = "Percent tip: ";
+        String lessThanZeroMessage = "You can't have a negative % tip. Try again.";
+
+        double percentTip = askDouble(prompt, lessThanZeroMessage);
+
+        percentTip /= 100;
+        return percentTip;
+    }
+
+    double calcTipPerPerson(double bill, int people, double percentTip) {
+        double tipPerPerson = (bill * percentTip) / people;
+        tipPerPerson = Math.ceil(tipPerPerson);
+
+        return tipPerPerson;
+    }
 
 }
--- HW2/MathGamesTester.java
+++ HW2/MathGamesTester.java
@@ -8,7 +8,7 @@ public class MathGamesTester {
         Scanner scan = new Scanner(System.in);
         MathGames math = new MathGames();
 
-        //Pythagorean Theorem Calculator
+        // Pythagorean Theorem Calculator
         System.out.println("\nPlease enter 2 side lengths of a triangle w/ sides a,b,and c.");
         System.out.println("(c is hypotenuse, enter 0 if side is not included)\n");
 
@@ -18,11 +18,22 @@ public class MathGamesTester {
         System.out.println();
 
         double missingSide = math.CalcMissingSide(a, b, c);
-        System.out.println("Length of Missing side: " + missingSide + "\n");
+        System.out.println("Length of missing side: " + missingSide + "\n");
 
-        //Grade Calculator
+        // Grade Calculator
         double askNum = math.askNum();
+        char grade = math.calcGrade(askNum);
 
+        System.out.println("Grade: " + grade + "\n");
+
+        //Tip on Bill per Person
+        double bill = math.askBill();
+        int people = math.askNumPeople();
+        double percentTip = math.askpercentTip();
+        System.out.println();
+
+        double tipPerPerson = math.calcTipPerPerson(bill, people, percentTip);
+        System.out.println("Tip per Person: $" + tipPerPerson);
 
         scan.close();
 
