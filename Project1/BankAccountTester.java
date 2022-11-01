d233a96c17fb957fb9371e3fe646ce0ff8dc7051
walshg3 <walshg3@go.stockton.edu>
2022-10-31 23:37:50 -0400

    added project1

---
+++ Project1/BankAccountTester.java
@@ -0,0 +1,9 @@
+package Project1;  
+public class BankAccountTester {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Bank Account Tester");
+        // Example Bank Account used for Transfer Methods
+        BankAccount gregChecking = new BankAccount(20000);
+
+    }
+}
